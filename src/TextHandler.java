bbe0b60d483c929ab85b6be71fdb161fa0c4323f
Karolord <karokodo123@gmail.com>
2022-03-21 13:37:51 +0300

    commit

---
+++ src/TextHandler.java
@@ -0,0 +1,103 @@
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class TextHandler implements Runnable {
+    private String s;
+    // private ReadWriteLock text = new ReentrantReadWriteLock();
+    private ReadWriteLock a1lock = new ReentrantReadWriteLock();
+    private ReadWriteLock a2lock = new ReentrantReadWriteLock();
+    private ReadWriteLock a3lock = new ReentrantReadWriteLock();
+    private int article1;
+    private int article2;
+    private int article3;
+
+    public TextHandler(String s) {
+        this.s = s.toLowerCase();
+    }
+
+    public int getArticle1() {
+        a1lock.readLock().lock();
+        try {
+            return this.article1;
+
+        } finally {
+            a1lock.readLock().unlock();
+        }
+    }
+
+    public void setArticle1(int article1) {
+        a1lock.writeLock().lock();
+        try {
+            this.article1 = article1;
+        } finally {
+            a1lock.writeLock().unlock();
+
+        }
+    }
+
+    public int getArticle2() {
+        a2lock.readLock().lock();
+        try {
+            return this.article2;
+
+        } finally {
+            a2lock.readLock().unlock();
+        }
+    }
+
+    public void setArticle2(int article2) {
+        a2lock.writeLock().lock();
+        try {
+            this.article2 = article2;
+        } finally {
+            a2lock.writeLock().unlock();
+
+        }
+    }
+
+    public int getArticle3() {
+        a3lock.readLock().lock();
+        try {
+            return this.article3;
+
+        } finally {
+            a3lock.readLock().unlock();
+        }
+    }
+
+    public void setArticle3(int article3) {
+        a3lock.writeLock().lock();
+        try {
+            this.article3 = article3;
+        } finally {
+            a3lock.writeLock().unlock();
+
+        }
+    }
+
+    @Override
+    public void run() {
+        int a1 = 0;
+        int a2 = 0;
+        int a3 = 0;
+        String[] words = s.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals("a")) {
+                a1++;
+            }
+            if (words[i].equals("an")) {
+                a2++;
+            }
+            if (words[i].equals("the")) {
+                a3++;
+            }
+        }
+        setArticle1(a1);
+        setArticle1(a2);
+        setArticle1(a3);
+
+        System.out.println(getArticle1());
+        System.out.println(getArticle2());
+        System.out.println(getArticle3());
+    }
+}
