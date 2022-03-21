0a9ebc8dbb23c3fdd0516149e36fe4a82609e4b6
Karolord <karokodo123@gmail.com>
2022-03-21 21:38:55 +0300

    commit

--- src/Cthread.java
+++ src/Cthread.java
@@ -9,38 +9,63 @@ public class Cthread extends Thread {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        super.run();
+
         for (int i = 0; i < arr.size(); i++) {
-            Thread t = new Thread(new TextPrinter(arr.get(i), "Thread " + (i + 1)));
-            t.start();
+            arr.get(i).text.writeLock().lock();
+            try {
+                arr.get(i).c.await();
+                System.out.println("a : " + arr.get(i).getArticle1() + " an : " + arr.get(i).getArticle2() + " The
+: " + arr.get(i).getArticle3());
+            } catch (Exception e) {
+                // TODO: handle exception
+            } finally {
+                arr.get(i).text.writeLock().unlock();
+            }
         }
     }
 }
+// public class Cthread extends Thread {
+// public ArrayList<TextHandler> arr = new ArrayList<TextHandler>();
 
-class TextPrinter implements Runnable {
-    public TextHandler t;
-    public String name;
+// public void addTextHandler(TextHandler t) {
+// arr.add(t);
+// }
 
-    public TextPrinter(TextHandler t, String name) {
-        this.t = t;
-        this.name = name;
-    }
+// @Override
+// public void run() {
+// // TODO Auto-generated method stub
+// super.run();
+// for (int i = 0; i < arr.size(); i++) {
+// Thread t = new Thread(new TextPrinter(arr.get(i), "Thread " + (i + 1)));
+// t.start();
+// }
+// }
+// }
 
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        t.text.writeLock().lock();
-        try {
-            System.out.println("Waiting");
-            t.c.await();
-            System.out.println(
-                    name + " : " + "a : " + t.getArticle1() + " an : " + t.getArticle2() + " The : " + t.getArticle3());
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            t.text.writeLock().unlock();
-        }
-    }
-}
+// class TextPrinter implements Runnable {
+// public TextHandler t;
+// public String name;
+
+// public TextPrinter(TextHandler t, String name) {
+// this.t = t;
+// this.name = name;
+// }
+
+// @Override
+// public void run() {
+// // TODO Auto-generated method stub
+// t.text.writeLock().lock();
+// try {
+// System.out.println("Waiting");
+// t.c.await();
+// System.out.println(
+// name + " : " + "a : " + t.getArticle1() + " an : " + t.getArticle2() + " The
+// : " + t.getArticle3());
+// } catch (InterruptedException e) {
+// // TODO Auto-generated catch block
+// e.printStackTrace();
+// } finally {
+// t.text.writeLock().unlock();
+// }
+// }
+// }
